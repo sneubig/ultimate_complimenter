97cc24d88297c427e26aefdf8837b361cd86127e
sneubig <neubig.stephen@gmail.com>
2014-09-03 20:22:47 -0400

    w05d03 hw

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -7,16 +7,19 @@ class NotificationsController < ApplicationController
   def create
     @notification = Notification.new(notification_params)
 
+    
+      @notification.text
+
     # Do some stuff that checks whether the Notification has
     # the info you need, then sends it! Otherwise, display errors
     # to the user!
 
-    redirect_to new_notification_path
-  end
+    redirect_to new_notification_path(@notification)
+    end
 
   private
   def notification_params
-    params.require(:notification).permit(:to_number)
+    params.permit(:to_number)
   end
 
 end
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -4,18 +4,24 @@ require_relative '../../credentials'
 class Notification
   attr_accessor :to_number
 
-  def initialize
+  def initialize(options = {})
+    self.to_number = options[:to_number]
     account_sid = Credentials.credentials[:twilio_account_sid]
     auth_token = Credentials.credentials[:twilio_account_token]
     @client = Twilio::REST::Client.new(account_sid, auth_token)
   end
 
   def text
-    # Sends a text to your phone number
+    contact = "5712967102"
+    notification = @client.account.messages.create(
+      :to => "5712967102",
+      :from => "+12405473623",
+      :body => random_compliment)
   end
 
   def random_compliment
-    # Generates a random compliment to send to the textee
+    compliments = ["Great job!", "You can do it!", "Have a great day!"]
+    compliments.sample
   end
 
 end
